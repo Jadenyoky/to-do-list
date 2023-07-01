30a0ec5ec3338cd521c672ad516a3a7ff5d8ad58
Jadenyoky <jaden_yoky@yahoo.com>
2023-07-01 21:28:51 +0300

    Finished To Do List React ..

--- src/App.js
+++ src/App.js
@@ -1,24 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Divider, TextField, Typography,  } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Brightness6, Close, LightModeOutlined, } from '@mui/icons-material';
 
-function App() {
+const App = () => {
+  const [mode, setmode] = useState(localStorage.getItem('moder') === null ? 'light' : localStorage.getItem('moder') === 'light' ? 'light' : 'dark');
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+
+  const ss = JSON.parse(localStorage.getItem('element'))
+
+  const [arrayText , setarrayText] = useState(localStorage.getItem('element') ? ss : [])
+  // const arrayText = localStorage.getItem('element') ? ss : []
+
+  const creating = () => {
+    const iText = document.querySelector('.inputText input')
+    while(iText.value !== ''){
+      console.log('not empty')
+
+      // arrayText.push({
+      //   id: Date.now(),
+      //   text: iText.value
+      // })
+
+      setarrayText([...arrayText, {
+        id: Date.now(),
+        text: iText.value
+      }])
+
+      // localStorage.setItem('element', JSON.stringify(arrayText))
+
+      // window.location.reload()
+
+      iText.value = ''
+
+      console.log(arrayText)
+    }
+  }
+
+  useEffect(()=>{
+    localStorage.setItem('element', JSON.stringify(arrayText))
+    console.log('ray')
+  },[arrayText])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Box sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        gap: '30px',
+        padding: '20px',
+      }}>
+        <TextField
+        variant='outlined'
+        sx={{
+          width: '300px',
+        }}
+        label='Do To List '
+        placeholder='Type Something to Add '
+        autoComplete='off'
+        className='inputText'
+        />
+        <Button 
+        variant='contained' color='primary'
+        onClick={()=> creating()}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Add
+        </Button>
+        <Button 
+        variant='contained'
+        color='secondary'
+        onClick={()=>{
+          localStorage.setItem('moder', darkTheme.palette.mode === 'light' ? 'dark' : 'light')
+          setmode(darkTheme.palette.mode === 'light' ? 'dark' : 'light')
+        }}
+        >
+        {
+          mode === 'light' ? <Brightness6 sx={{ color: 'white' }} /> : <LightModeOutlined sx={{ color: 'white' }} />
+        }
+      </Button>
+      </Box>
+      <Divider />
+        
+      <div className='aniAll'>
+        {
+          arrayText.map((e,k)=>{
+            return (
+              <div key={k} className='aniEle' >
+                <Box sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center' ,
+                  padding: '20px',
+                  gap: '20px',
+                }}>
+                  <Typography variant='h5' sx={{
+                    flexGrow: '2'
+                  }}>
+                    {e.text}
+                  </Typography>
+                  <Button
+                  color='error'
+                  variant='contained'
+                  onClick={()=>{
+                    setarrayText(arrayText.filter((item)=> item.id !== e.id ))
+                  }}
+                  >
+                    <Close />
+                  </Button>
+                </Box>
+                <Divider/>
+              </div>
+            )
+          })
+        }
+      </div>
+
+
+    </ThemeProvider>
   );
 }
 
